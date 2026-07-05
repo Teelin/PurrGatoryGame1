b5c0991225e7498c6ae939e7f660fd4aea625059
Teelin <calum.teeling@googlemail.com>
2026-07-05 17:09:03 +0100

    Enemy 3 + extras
    Enemy 3 now in just not attacking yet.
    Bast animations now in with new added lighting and animated lights

--- PurrGatory/Assets/Scripts/Enemy3.cs
+++ PurrGatory/Assets/Scripts/Enemy3.cs
@@ -1,11 +1,14 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy3 : MonoBehaviour
 {
     [SerializeField]  GameObject[] patrolPoints ;
+    int targetPointIndex = 0;
     [SerializeField] float patrolSpeed;
+    bool changingTarget = false;
 
     private void Start()
     {
@@ -14,21 +17,46 @@ public class Enemy3 : MonoBehaviour
             point.transform.parent = null; // Detach patrol points from the enemy
         }
     }
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        for(int i = 0; i < patrolPoints.Length; )
+        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[targetPointIndex].transform.position, patrolSpeed * Time.deltaTime);
+        if (patrolPoints[targetPointIndex].transform.position == transform.position && !changingTarget)
         {
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[i].transform.position, patrolSpeed * Time.deltaTime);
-            if (patrolPoints[i].transform.position == transform.position)
-            {
-                i++;
-                if (i >= patrolPoints.Length)
-                {
-                    i = 0;
-                }
-            }
+            changingTarget = true;
+            StartCoroutine(ChangeTarget());
+            
+        }
+    }
+
+    IEnumerator ChangeTarget()
+    {
+        yield return new WaitForSeconds(1f);
+        targetPointIndex++;
+        if (targetPointIndex >= patrolPoints.Length)
+        {
+            targetPointIndex = 0;
+        }
+        changingTarget = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            // Handle collision with player
+            Debug.Log("Enemy collided with Player!");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // Handle trigger with player
+            Debug.Log("Enemy triggered with Player!");
         }
     }
 }
+
+
 
